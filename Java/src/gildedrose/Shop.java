95e9cc5a06cd21d6f05f886fda6750b97fbbc4f5
EbbTechnologies <marius@ebbtechnologies.com>
2022-10-24 22:45:49 +0200

    Correctly configured Java project

---
+++ Java/src/gildedrose/Shop.java
@@ -0,0 +1,5 @@
+package gildedrose;
+
+public class Shop {
+
+}
--- Java/test/GildedRoseTest.java
+++ Java/test/GildedRoseTest.java
@@ -1,14 +1,13 @@
-package com.gildedrose;
+import gildedrose.Shop;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-
     @Test
     void should_build() {
-        assertTrue(true);
+        Shop shop = new Shop();
+        assertEquals(shop.getClass(), Shop.class);
     }
-
 }
